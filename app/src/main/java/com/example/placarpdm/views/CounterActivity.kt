423a718f4e4a44eb391b0036a9db6d4e075cfc9a
IsraelCueio <israelcoelhocunhaccc@gmail.com>
2024-08-19 18:32:23 -0300

    placar points counter implemented

--- app/src/main/java/com/example/placarpdm/views/CounterActivity.kt
+++ app/src/main/java/com/example/placarpdm/views/CounterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.placarpdm.views
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +20,24 @@ class CounterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_counter)
+        val housePointsElement: TextView = findViewById(R.id.housePoints)
+        val housePointsPlus: Button = findViewById(R.id.houseButton)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        housePointsPlus.setOnClickListener {
+            var currentHousePoints = housePointsElement.text.toString().toInt()
+            currentHousePoints += 1
+            housePointsElement.setText(currentHousePoints.toString())
+        }
+        val adversaryPointsElement: TextView = findViewById(R.id.adversaryPoints)
+        val adversaryPointsPlus: Button = findViewById(R.id.adversaryButton)
+        adversaryPointsPlus.setOnClickListener {
+            var currentAdversaryPoints = adversaryPointsElement.text.toString().toInt()
+            currentAdversaryPoints += 1
+            adversaryPointsElement.setText(currentAdversaryPoints.toString())
         }
     }
 }
